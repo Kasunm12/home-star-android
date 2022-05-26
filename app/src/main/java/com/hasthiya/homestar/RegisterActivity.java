48ba9a28790306a767804f6923957298aaa5ee86
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2022-05-26 15:32:01 +0530

    create navigation

--- app/src/main/java/com/hasthiya/homestar/RegisterActivity.java
+++ app/src/main/java/com/hasthiya/homestar/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.hasthiya.homestar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        signup =  findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
