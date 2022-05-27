d230420bffc73942e1620f6119ef14bec406ede8
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2022-05-27 16:20:45 +0530

    forget password api done

--- app/src/main/java/com/hasthiya/homestar/ui/ForgetPasswordActivity.java
+++ app/src/main/java/com/hasthiya/homestar/ui/ForgetPasswordActivity.java
@@ -2,20 +2,61 @@ package com.hasthiya.homestar.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.hasthiya.homestar.R;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
 
-    EditText ForgetEmail;
+    EditText ForgetEmail,subject,emailbody;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+        ForgetEmail = (EditText)findViewById(R.id.ForgetEmail);
         setTitle("Forgot Password");
+        ForgetEmail.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                ForgetPasswordActivity.this.finish();
+                Intent intent = new Intent();
+                intent.setClass(v.getContext(), LogInActivity.class);
+                startActivity(intent);
+
+            }
+            //Code For Sending mail
+
+        });
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendEmail();
+            }
+
+            private void sendEmail() {
+
+                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"testmail@testmail.com"});
+                emailIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject.getText());
+                emailIntent.putExtra(android.content.Intent.EXTRA_TEXT, emailbody.getText());
+                ForgetPasswordActivity.this.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+
+            }
+        });
+
+
+
     }
+
 
 }
--- app/src/main/java/com/hasthiya/homestar/ui/RegisterActivity.java
+++ app/src/main/java/com/hasthiya/homestar/ui/RegisterActivity.java
@@ -6,12 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.hasthiya.homestar.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button signup;
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
